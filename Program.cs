20df446c214f46563d85fea83091fd2b3bbefe82
Juliana Moreira <omoreiraju@gmail.com>
2024-04-05 20:51:38 -0300

    Matriz e loop

--- Program.cs
+++ Program.cs
@@ -1,30 +1,74 @@
 ﻿using System.Runtime.InteropServices;
 
-Console.WriteLine("Listas");
-List<string> nomes = new List<string>();
-// // Add nomes na lista
-// nomes.Add("Ju");
-// nomes.Add("Su");
-// nomes.Add("Fu");
-// nomes.Add("Hu");
-// nomes.Add("Au");
+  //-----//
+ //WHILE//
+//-----//
 
-// //remov nomes da lista
-// nomes.Remove("Su");
+// Console.WriteLine("Bem vindo(a) ao jogo da adivinha.");
 
-// //Acessando pelo índice
-// Console.WriteLine(nomes.ElementAt(2));
+// int numero = 15;
+// repeat1:
+// Console.Write("Adivinhe o número secreto: ");
+// int resposta = int.Parse(Console.ReadLine());
 
-// //Saída
-// nomes.ForEach(Console.WriteLine);
+// while(resposta != numero){
+//     Console.WriteLine("Poxa.. você errou :( \nTente novamente: )");
+//     goto repeat1;
+// }
 
-// //Contando qtd elementos
-// Console.WriteLine(nomes.Count());
+// Console.Clear();
+// Console.WriteLine($"Parabéns, você acertou! \nO número secreto realmente era {numero}!! :)");
 
-List<int> ListaNum = new List<int>{10, 20, 30, 40, 100};
-ListaNum.RemoveAt(1);
-ListaNum.ForEach(Console.WriteLine);
+  //------//
+ //MATRIZ//
+//------//
 
+Console.WriteLine("Matrizes => L,C ");
+
+//declaração
+string[,] Boletim;
+//instanciação
+Boletim = new string[5,5];
+
+//linha 0
+Boletim[0,0] = "Aluno";
+Boletim[0,1] = "PORT";
+Boletim[0,2] = "MAT";
+Boletim[0,3] = "ART";
+Boletim[0,4] = "EdF";
+//linha 1
+Boletim[1,0] = "5,0";
+Boletim[1,1] = "8,0";
+Boletim[1,2] = "7,5";
+Boletim[1,3] = "9,5";
+Boletim[1,4] = "7,0";
+//linha 2
+Boletim[2,0] = "5,0";
+Boletim[2,1] = "9,0";
+Boletim[2,2] = "8,0";
+Boletim[2,3] = "7,0";
+Boletim[2,4] = "10,0";
+//linha 3
+Boletim[3,0] = "4,5";
+Boletim[3,1] = "3,0";
+Boletim[3,2] = "9,0";
+Boletim[3,3] = "10,0";
+Boletim[3,4] = "10,0";
+//linha 4
+Boletim[4,0] = "5,0";
+Boletim[4,1] = "2,8";
+Boletim[4,2] = "4,2";
+Boletim[4,3] = "9,2";
+Boletim[4,4] = "3,3";
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write($"{Boletim[i,j]}\t");
+    }
+    Console.WriteLine();
+}
 
 
 //
